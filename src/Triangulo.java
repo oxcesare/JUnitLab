48aae059bd4428846b9479d5002e9d0d0b75c2da
cesar <cesar180687@hotmail.com>
2012-11-05 11:17:33 -0600

    Ultimo Ejercicio

--- src/Triangulo.java
+++ src/Triangulo.java
@@ -21,6 +21,47 @@ public class Triangulo {
 	public int getC(){
 		return c;
 	}
+	
+	boolean todosPositivos(){
+		return a > 0 && b > 0 && c > 0;
+	}
+	
 
+	public boolean satisfaceDesigualdad(){
+		return  (a < (b+c))  && (b < (a+c)) && (c < (b+a));
+	}
+	
+	public boolean esValido(){
+		return todosPositivos();
+	}
+	
+	
+	public String queTipoTrianguloEs(int a, int b, int c){
+		String Cadena="";
+		
+		if (a == b && b == c && a == c) {
+			
+			  Cadena="Equilatero";
+		}
+				
+		if (a == b || b == c || a == c )
+		{
+		   Cadena="Isosceles";
+		}
+		
+		if (a != b && b != c && a != c)
+		{
+		   Cadena="Escaleno";
+		}
+		
+		if (a != b || b != c || a != c)
+		{
+		   Cadena="No es un Triangulo";
+		}
+		
+			
+		return Cadena;
+		
+	}
 	
 }
--- src/TrianguloTest.java
+++ src/TrianguloTest.java
@@ -1,6 +1,12 @@
 import junit.framework.TestCase;
 
 
+//metodo
+//planear pruebas
+//implementar pruebas
+//Equilatero, Isosceles, Escaleno, No es un Triangulo
+
+
 public class TrianguloTest extends TestCase {
 	
 	public static void main (String ars[]){
@@ -16,8 +22,50 @@ public class TrianguloTest extends TestCase {
 	}
 	
 	public void testEsValido(){
-		Triangulo t = new Triangulo(-5,3,7);
+		Triangulo t = null;
+		//assertFalse(t.esValido());
+		
+		t = new Triangulo(-5,3,7);
 		assertFalse(t.esValido());
+		
+		t = new Triangulo(3,5,7);
+		assertTrue(t.esValido());
+		
 	}
+	
+	public void testSatisfaceDesigualdad(){
+		Triangulo t = null;
+		
+		t = new Triangulo(10,5,2);
+		assertFalse(t.satisfaceDesigualdad());
+		
+		t = new Triangulo(5,15,2);
+		assertFalse(t.satisfaceDesigualdad());
+		
+		t = new Triangulo(3,4,7);
+		assertFalse(t.satisfaceDesigualdad());
+		
+		t = new Triangulo(5,6,3);
+		assertTrue(t.satisfaceDesigualdad());
+	}
+	
+	public void tipo(){
+		Triangulo t = null;
+		
+		t = new Triangulo (5,5,5);
+		assertEquals(1,t.queTipoTrianguloEs(5,5,5));
+
+		
+		t = new Triangulo (5,5,1);
+		assertEquals(1,t.queTipoTrianguloEs(5,5,7));
+		
+		
+		t = new Triangulo(5,7,12);
+		assertEquals(1,t.queTipoTrianguloEs(5,7,12));
+				
+	}
+	
+	
 
 }
+
